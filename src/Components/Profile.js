f2af4412cf4de1e01690fe241640257c80b0bee6
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2022-06-06 17:49:13 -0300

    implantação toggle menu

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {CgProfile} from 'react-icons/cg';
 import "./Profile.css";
 
-const Profile = () => {
+const Profile = (props) => {
     return (
         <div className="profile">
             <CgProfile className="profile__icon" />
-            <h1 className="profile__title">Gabriel Gomes</h1>
+            {props.menu === false?<h1 className="profile__title">Gabriel Gomes</h1>:"" }
         </div>
     );
 }
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,27 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Menu.css";
 import Profile from "../Components/Profile";
 import {AiOutlineHome, AiOutlineFolderOpen} from "react-icons/ai"
 import {BsInfoCircle} from "react-icons/bs"
 import {MdOutlineContactPhone} from "react-icons/md"
+import {GiHamburgerMenu} from "react-icons/gi"
+import {GrClose} from "react-icons/gr"
 
 
 const Menu = () => {
+    const[menu, setMenu] = useState(false);    
     return(
-        <div className="menu">
-            <Profile />
+        <div className={menu === false?"menu":"menu-hidden"}>
+            {menu === false?<GrClose onClick={() => setMenu(!menu)} className="menu-icon"/>:<GiHamburgerMenu onClick={() => setMenu(!menu)} className="menu-icon"/>}
+            
+            <Profile menu={menu} />
             <div className="menu-list">
                 <div className="list-items">
-                    <h4>Início</h4><AiOutlineHome /> 
+                    {menu===false?<><h4 className="menu-items">Início</h4><AiOutlineHome /></>:<AiOutlineHome className="menu-items-icons" />} 
                 </div>
                 <div className="list-items">
-                    <h4>Sobre</h4><BsInfoCircle />
+                    {menu===false?<><h4 className="menu-items">Sobre</h4><BsInfoCircle /></>:<BsInfoCircle className="menu-items-icons" />}
                 </div>
                 <div className="list-items">
-                    <h4>Portifólio</h4><AiOutlineFolderOpen />
+                    {menu===false?<><h4 className="menu-items">Portifólio</h4><AiOutlineFolderOpen /></>:<AiOutlineFolderOpen className="menu-items-icons" />}
                 </div>
                 <div className="list-items">
-                    <h4>Contato</h4><MdOutlineContactPhone />
+                    {menu===false?<><h4 className="menu-items">Contato</h4><MdOutlineContactPhone /></>:<MdOutlineContactPhone className="menu-items-icons" />}
                 </div>  
             </div>
         </div>
